a1ccf9d888161ac6ff689c992a3940c4ab2565f3
Jeff Lindsay <progrium@gmail.com>
2016-08-09 12:26:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const Promise = require("bluebird");
+
+const pleaseRebase = "Please rebase your PR, thanks!";
 
 function onPush(github, event, cb) {
   console.log("onPush");
@@ -5,20 +8,59 @@ function onPush(github, event, cb) {
     console.log("Not master");
     cb();
   }
-  console.log("Fetching PRs...");
+  Promise.promisifyAll(github);
   github.pullRequests.getAll({
     user: event.repository.owner.name,
     repo: event.repository.name,
     base: "master"
-    
-  }, function(err, res) {
-    if (err) {
-      console.log(err.message);
-      cb();
-      return;
+  
+  }).then(function(prs) {
+    if (prs.length == 0) {
+      console.log("No PRs");
+      return cb();
     }
-    console.log("Got PRs...");
-    console.log(JSON.stringify(res));
-    cb();
+    Promise.all(
+      prs.map(function(pr) {
+        return github.pullRequests.get({
+          user: pr.base.repo.owner.login,
+          repo: pr.base.repo.name,
+          number: pr.number
+        })
+      })
+    ).then(function(prs) {
+      Promise.all(
+        prs.map(function(pr) {
+          if (pr.mergeable == false) {
+            return github.issues.getComments({
+              user: pr.base.repo.owner.login,
+              repo: pr.base.repo.name,
+              number: pr.number,
+              per_page: 100
+            }).then(function(comments) {
+              if (comments.length > 0 && comments[comments.length-1].body == pleaseRebase) {
+                return Promise.resolve();
+              }
+              return github.issues.createComment({
+                user: pr.base.repo.owner.login,
+                repo: pr.base.repo.name,
+                number: pr.number,
+                body: pleaseRebase
+              });
+            });
+          } else {
+            return Promise.resolve();
+          }
+        })
+      ).then(function() {
+        console.log("done!");
+        cb();
+      });
+    });
+  }).catch(function(err) {
+    if (err) {
+      console.log("error:", err.message);
+      return cb();
+    }
   });
 }
+
