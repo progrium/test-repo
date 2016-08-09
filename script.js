46f5cbf1d3bb6f0803710c0cbfa178f120c50fb8
Jeff Lindsay <progrium@gmail.com>
2016-08-09 09:41:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 
 function onPush(github, event, cb) {
-  console.log("Hello");
+  if (event.ref != "refs/heads/master") {
+    cb();
+  }
+  github.pullRequests.getAll({
+    user: event.repo.owner.login,
+    repo: event.repo.name,
+    base: "master"
+    
+  }, function(err, res) {
+    console.log(JSON.stringify(res));
+  });
   cb();
 }
